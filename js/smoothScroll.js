347cb4bca544f8768c5d8847e8e3eace31dd5477
Amitoshdeep <104307738+Amitoshdeep@users.noreply.github.com>
2025-01-12 20:23:58 +0530

    Delete js directory

--- js/smoothScroll.js
+++
@@ -1,2 +0,0 @@
-document.documentElement.style.overflowX = "hidden"; // Disable horizontal scrolling
-document.documentElement.style.scrollBehavior = "smooth"; // Ensure smooth scrolling remains active
