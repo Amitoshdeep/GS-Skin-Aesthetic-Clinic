02ff6ef81d1444c29880165f6ddcacf3826b0304
Amitoshdeep <104307738+Amitoshdeep@users.noreply.github.com>
2025-01-12 20:24:50 +0530

    Add files via upload

---
+++ js/smoothScroll.js
@@ -0,0 +1,2 @@
+document.documentElement.style.overflowX = "hidden"; // Disable horizontal scrolling
+document.documentElement.style.scrollBehavior = "smooth"; // Ensure smooth scrolling remains active
